5746417105d6649ffb939e7fd1fd2d7b4df8012e
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-02-27 00:36:32 +0900

    Add avatarUrl

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String }
+  password: { type: String },
+  avatarUrl: {
+    type: String,
+    default:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  }
 });
 
 const User = mongoose.model("User", userSchema);
